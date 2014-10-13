b7103b139c58edec835232169c4cec0c7b06edde
xcitic <xcitic@gmail.com>
2014-10-12 23:18:17 -0600

    Forum Fixed

--- lab/cache/log.php
+++ lab/cache/log.php
@@ -78,3 +78,51 @@ Session "default" started at 2014-10-13T04:39:57+0000
 #6 ERROR: Error: include_once(): Failed opening '/home8/xciticvi/public_html/evotvo/wp-content/sunrise.php' for inclusion (include_path='/home8/xciticvi/public_html/evotvo/lab/library/pdf:/home8/xciticvi/public_html/evotvo/lab/library/PEAR:/home8/xciticvi/public_html/evotvo/lab/library/ezcomponents:/home8/xciticvi/public_html/evotvo/lab/library/zend:/home8/xciticvi/public_html/evotvo/lab:/home8/xciticvi/public_html/evotvo/lab/application:.:/usr/php/54/usr/lib64:/usr/php/54/usr/share/pear') in '/home8/xciticvi/public_html/evotvo/wp-includes/ms-settings.php' on line 18 (error code: 2)
 Time since start: 1.8083491325378 seconds
 -------------------------------------------------------------------------------
+
+Session "default" started at 2014-10-13T04:41:34+0000
+#1 ERROR: Error: Constant DB_NAME already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 20 (error code: 8)
+#2 ERROR: Error: Constant DB_USER already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 23 (error code: 8)
+#3 ERROR: Error: Constant DB_HOST already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 29 (error code: 8)
+#4 ERROR: Error: Constant DB_CHARSET already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 32 (error code: 8)
+Time since start: 0.63857889175415 seconds
+-------------------------------------------------------------------------------
+
+Session "default" started at 2014-10-13T04:46:20+0000
+#1 ERROR: Error: Constant DB_NAME already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 20 (error code: 8)
+#2 ERROR: Error: Constant DB_USER already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 23 (error code: 8)
+#3 ERROR: Error: Constant DB_HOST already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 29 (error code: 8)
+#4 ERROR: Error: Constant DB_CHARSET already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 32 (error code: 8)
+Time since start: 0.88568210601807 seconds
+-------------------------------------------------------------------------------
+
+Session "default" started at 2014-10-13T04:46:26+0000
+#1 ERROR: Error: Constant DB_NAME already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 20 (error code: 8)
+#2 ERROR: Error: Constant DB_USER already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 23 (error code: 8)
+#3 ERROR: Error: Constant DB_HOST already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 29 (error code: 8)
+#4 ERROR: Error: Constant DB_CHARSET already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 32 (error code: 8)
+Time since start: 0.69986510276794 seconds
+-------------------------------------------------------------------------------
+
+Session "default" started at 2014-10-13T04:48:54+0000
+#1 ERROR: Error: Constant DB_NAME already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 20 (error code: 8)
+#2 ERROR: Error: Constant DB_USER already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 23 (error code: 8)
+#3 ERROR: Error: Constant DB_HOST already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 29 (error code: 8)
+#4 ERROR: Error: Constant DB_CHARSET already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 32 (error code: 8)
+Time since start: 0.59696984291077 seconds
+-------------------------------------------------------------------------------
+
+Session "default" started at 2014-10-13T04:53:42+0000
+#1 ERROR: Error: Constant DB_NAME already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 20 (error code: 8)
+#2 ERROR: Error: Constant DB_USER already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 23 (error code: 8)
+#3 ERROR: Error: Constant DB_HOST already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 29 (error code: 8)
+#4 ERROR: Error: Constant DB_CHARSET already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 32 (error code: 8)
+Time since start: 0.86792707443237 seconds
+-------------------------------------------------------------------------------
+
+Session "default" started at 2014-10-13T04:55:22+0000
+#1 ERROR: Error: Constant DB_NAME already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 20 (error code: 8)
+#2 ERROR: Error: Constant DB_USER already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 23 (error code: 8)
+#3 ERROR: Error: Constant DB_HOST already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 29 (error code: 8)
+#4 ERROR: Error: Constant DB_CHARSET already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 32 (error code: 8)
+Time since start: 1.1438710689545 seconds
+-------------------------------------------------------------------------------
