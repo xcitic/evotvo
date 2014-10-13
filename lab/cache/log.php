8a9dc786e67cec1112d2273fcbd3628f1e80b674
xcitic <xcitic@gmail.com>
2014-10-12 22:40:33 -0600

    DomainMapping

--- lab/cache/log.php
+++ lab/cache/log.php
@@ -68,3 +68,13 @@ Session "default" started at 2014-10-13T04:24:53+0000
 #6 ERROR: Error: include_once(): Failed opening '/home8/xciticvi/public_html/evotvo/wp-content/sunrise.php' for inclusion (include_path='/home8/xciticvi/public_html/evotvo/lab/library/pdf:/home8/xciticvi/public_html/evotvo/lab/library/PEAR:/home8/xciticvi/public_html/evotvo/lab/library/ezcomponents:/home8/xciticvi/public_html/evotvo/lab/library/zend:/home8/xciticvi/public_html/evotvo/lab:/home8/xciticvi/public_html/evotvo/lab/application:.:/usr/php/54/usr/lib64:/usr/php/54/usr/share/pear') in '/home8/xciticvi/public_html/evotvo/wp-includes/ms-settings.php' on line 18 (error code: 2)
 Time since start: 0.63337016105652 seconds
 -------------------------------------------------------------------------------
+
+Session "default" started at 2014-10-13T04:39:57+0000
+#1 ERROR: Error: Constant DB_NAME already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 20 (error code: 8)
+#2 ERROR: Error: Constant DB_USER already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 23 (error code: 8)
+#3 ERROR: Error: Constant DB_HOST already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 29 (error code: 8)
+#4 ERROR: Error: Constant DB_CHARSET already defined in '/home8/xciticvi/public_html/evotvo/wp-config.php' on line 32 (error code: 8)
+#5 ERROR: Error: include_once(/home8/xciticvi/public_html/evotvo/wp-content/sunrise.php): failed to open stream: No such file or directory in '/home8/xciticvi/public_html/evotvo/wp-includes/ms-settings.php' on line 18 (error code: 2)
+#6 ERROR: Error: include_once(): Failed opening '/home8/xciticvi/public_html/evotvo/wp-content/sunrise.php' for inclusion (include_path='/home8/xciticvi/public_html/evotvo/lab/library/pdf:/home8/xciticvi/public_html/evotvo/lab/library/PEAR:/home8/xciticvi/public_html/evotvo/lab/library/ezcomponents:/home8/xciticvi/public_html/evotvo/lab/library/zend:/home8/xciticvi/public_html/evotvo/lab:/home8/xciticvi/public_html/evotvo/lab/application:.:/usr/php/54/usr/lib64:/usr/php/54/usr/share/pear') in '/home8/xciticvi/public_html/evotvo/wp-includes/ms-settings.php' on line 18 (error code: 2)
+Time since start: 1.8083491325378 seconds
+-------------------------------------------------------------------------------
